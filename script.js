e07072cf3d52a0549dcc2967f945394909de8fd8
jun <hliao20@my.bcit.ca>
2020-02-11 14:53:32 -0800

    lab 4

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var artistList = [];
+// localStorage.clear();
+loadStorage();
+
 function addArtist() {
 let addArtistbtn = document.querySelector("#add-artist");
 let form = document.querySelector("#add-artist-form");
@@ -8,38 +12,139 @@ let form = document.querySelector("#add-artist-form");
         form.style.display = "none";
         addArtistbtn.textContent = "Add Artist";
     }
-    console.log("1");
 }
 
 function addedArtist(){
-    console.log("2");
-    addArtist();
     let name = document.querySelector("#name").value;
     let des = document.querySelector("#description").value;
     let url = document.querySelector("#url").value;
+
+    if(checkExistArtist(name)){
+        window.alert("The artist exists in the dictionary.");
+        return;
+    }
+
+    artistList.push({"name" : name, "description" : des, "link": url});
+
     let cell = document.createElement("div");
     let list = document.getElementById("list");
     let textContent = document.createElement("div");
     let deletebtn = document.createElement("button");
-    cell.className = "cell";
     let artistName = document.createElement("h3");
+    let artistDes = document.createElement("p");
+    let icon = document.createElement("img");
+
+    cell.className = "cell";
     artistName.textContent = name;
     artistName.className = "artist-name"
-    let artistDes = document.createElement("p");
     artistDes.textContent = des;
     artistDes.className = "artist-description";
-    let icon = document.createElement("img");
     icon.className = "image"
     textContent.className = "content";
     deletebtn.className = "delete";
     deletebtn.textContent = "delete";
     icon.setAttribute("src", url);
+
+    localStorage.setItem("artist-list", JSON.stringify(artistList));
+
+    location.reload();
+
+    // cell.appendChild(icon);
+    // textContent.appendChild(artistName);
+    // textContent.appendChild(artistDes);
+    // cell.appendChild(textContent);
+    // cell.appendChild(deletebtn);
+    // list.appendChild(cell);
+    // deletebtn.onclick = (e) => {
+    //     deleteArtist(deletebtn);
+    // }
+
+    
+}
+
+function deleteArtist(deletebtn){
+    let parent = deletebtn.parentNode;
+    console.log(parent.childNodes);
+    for (var i = 0; i < parent.childNodes.length; i++){
+        if (parent.childNodes[i].className == "content"){
+            var delName = parent.children[i].children[0].textContent;
+            var list = JSON.parse(localStorage.getItem("artist-list"));
+            for(let i=0; i<list.length; i++){
+                console.log(list[i]);
+                if (list[i].name == delName){
+                    list.splice(i,1);
+                    break;
+                }
+            }
+            localStorage.setItem("artist-list", JSON.stringify(list));
+            deletebtn.parentNode.parentNode.removeChild(deletebtn.parentNode);
+            break
+        }
+    }
+}
+function search(){
+    var a = document.getElementById("search").value.toUpperCase();
+    var list = JSON.parse(localStorage.getItem("artist-list"));
+    var cell = document.getElementsByClassName("cell");
+
+    for(let i=0; i<list.length; i++){
+        if(!list[i].name.toUpperCase().includes(a)){
+            cell[i].style.display = "none";
+        } else {
+            cell[i].style.display = "flex";
+        }
+    }
+}
+
+function loadStorage(){
+    if (localStorage.getItem("artist-list") == null)
+        return;
+    let list = localStorage.getItem("artist-list");
+    artistList = JSON.parse(list);
+    artistList.forEach(function(a){
+        loadArtist(a["name"], a["description"], a["link"]);
+    }
+    )
+}
+
+function loadArtist(name, des, url){
+    let cell = document.createElement("div");
+    let list = document.getElementById("list");
+    let textContent = document.createElement("div");
+    let deletebtn = document.createElement("button");
+    let artistName = document.createElement("h3");
+    let artistDes = document.createElement("p");
+    let icon = document.createElement("img");
+
+    cell.className = "cell";
+    artistName.textContent = name;
+    artistName.className = "artist-name"
+    artistDes.textContent = des;
+    artistDes.className = "artist-description";
+    icon.className = "image"
+    textContent.className = "content";
+    deletebtn.className = "delete";
+    deletebtn.textContent = "delete";
+    icon.setAttribute("src", url);
+
     cell.appendChild(icon);
     textContent.appendChild(artistName);
     textContent.appendChild(artistDes);
     cell.appendChild(textContent);
     cell.appendChild(deletebtn);
     list.appendChild(cell);
-    console.log(0);
-    deletebtn.onclick = (e) => deletebtn.parentNode.parentNode.removeChild(deletebtn.parentNode);
+    deletebtn.onclick = (e) => {
+        deleteArtist(deletebtn);
+        location.reload();
+    }
+}
+
+function checkExistArtist(name){
+    var list = document.getElementById("list");
+    for(let i=0; i < list.children.length; i++){
+        if(list.children[i].children[1].children[0].textContent.localeCompare(name) == 0){
+            return true;
+        }
+    }
+    return false;
 }
